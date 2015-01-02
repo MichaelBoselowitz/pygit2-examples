45a72eb20ee03dbf98ea2faee971e67fd8c71cd6
Michael Boselowitz <boselowitz@maya.com>
2015-01-02 18:55:56 -0500

    Adding basic pull functionality.
    Pulling is not currently working well for the fast forward case. It
    however seems to be functioning fine for the more difficult normal
    merging case. Go figure.

--- examples.py
+++ examples.py
@@ -30,6 +30,7 @@ def create_commits(repo, how_many):
     for i in range(how_many):
         test_fp = open(os.path.join(repo.workdir,
                                     os.path.basename(os.path.normpath(repo.workdir)) + '_test.txt'), 'a+')
+        print 'Writing %s to %s on %s repo' % ('Version %d.\n' % (version), test_fp.name, repo)
         test_fp.write('Version %d.\n\n' % (version))
         test_fp.close()
         repo.index.add_all()
@@ -44,8 +45,43 @@ def create_commits(repo, how_many):
                                     parent)
         parent = [commit]
         version += 1
-    
-# Repo init
+
+
+def pull(repo, remote_name='origin'):
+    for remote in repo.remotes:
+        if remote.name == remote_name:
+            remote.fetch()
+            remote_master_id = repo.lookup_reference('refs/remotes/origin/master').target
+            merge_result, _ = repo.merge_analysis(remote_master_id)
+            # Up to date, do nothing
+            if merge_result & pygit2.GIT_MERGE_ANALYSIS_UP_TO_DATE:
+                return
+            # We can just fastforward
+            elif merge_result & pygit2.GIT_MERGE_ANALYSIS_FASTFORWARD:
+                print repo.head.target
+                print repo.status()
+                master_ref = repo.lookup_reference('refs/heads/master')
+                master_ref.set_target(remote_master_id)
+                repo.head.set_target(remote_master_id)
+                repo.checkout_head()
+                print repo.status()
+            elif merge_result & pygit2.GIT_MERGE_ANALYSIS_NORMAL:
+                repo.merge(remote_master_id)
+                print repo.index.conflicts
+
+                assert repo.index.conflicts is None, 'Conflicts, ahhhh!'
+                user = repo.default_signature
+                tree = repo.index.write_tree()
+                commit = repo.create_commit('HEAD',
+                                            user,
+                                            user,
+                                            'Merge!',
+                                            tree,
+                                            [repo.head.target, remote_master_id])
+                repo.state_cleanup()
+            else:
+                raise AssertionError('Unknown merge analysis result')
+
 # Initialize new remote repo
 remote_repo = pygit2.init_repository(REMOTE_REPO, False)
 create_commits(remote_repo, 1)
